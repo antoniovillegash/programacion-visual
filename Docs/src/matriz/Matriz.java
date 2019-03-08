c4d99645d1b2869a98db9ccf4bc702056e0ca4b6
Luis Antonio Villegas Hernández <antoniovillegash>
2019-03-08 11:15:10 -0600

    examen diagnostico update

--- Docs/src/matriz/Matriz.java
+++ Docs/src/matriz/Matriz.java
@@ -2,11 +2,20 @@ package matriz;
 
 public class Matriz {
 	public static void main(String[] args) {
-		int[][] matriz={{1,3,5,7},{4,7,9,7},{2,6,8,0},{2,4,5,2}};
+		int[][] matriz={{1,3,5,7},
+						{4,7,9,7},
+						{2,6,8,0},
+						{2,4,5,2}};
 	
 		for(int i=0; i<4; i++) {
 			for(int j=0; j<4; j++) {
-				System.out.print(matriz[j][i]);
+				if(i%2==0) {
+					System.out.print(matriz[j][i]);
+				}else {
+					j=3-j;
+					System.out.print(matriz[j][i]);
+				}
+				
 			}
 			
 		}
